a8c9d4219eedcce74d4a047a59cf9ca8d7d25ec6
Daniel Cintra <cintra.70@gmail.com>
2019-10-02 20:13:05 -0300

    Retoques finais

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,7 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_fitness_app/widgets/header.dart';
-import '../models/food.dart';
 import '../widgets/list_foods.dart';
-// import '../widgets/header.dart';
+import '../widgets/header.dart';
 import '../BLoC/food_bloc.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,7 +10,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<Food> originalFoods;
   final FoodBloc bloc = BlocProvider.getBloc<FoodBloc>();
 
   @override
